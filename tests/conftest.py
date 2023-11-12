6d674cb061704e121d0485919d8d7b50cd0f49d0
BohdanObruch <bodan1992@gmail.com>
2023-11-12 08:35:40 +0300

    add yml file and change conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,36 +30,50 @@ def pytest_addoption(parser):
         '--browser_version',
         default='116.0'
     )
+    parser.addoption(
+        '--headless',
+        default=False
+    )
 
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
     browser_version = request.config.getoption('--browser_version')
+    headless = request.config.getoption('--headless')
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-setuid-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
+
+    if headless == 'True':
+        options.add_argument('--headless')
+        browser.config.browser_name = 'chrome'
+        browser.config.timeout = float(os.getenv('selene.timeout', '4'))
+        browser.config.window_width = 1920
+        browser.config.window_height = 1080
+
+    else:
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": browser_version,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
         }
-    }
-    options.capabilities.update(selenoid_capabilities)
+        options.capabilities.update(selenoid_capabilities)
 
-    url = os.getenv('URL')
+        url = os.getenv('URL')
 
-    driver = webdriver_selenium.Remote(
-        command_executor=f"{url}/wd/hub",
-        options=options
-    )
-    browser.config.driver = driver
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+        driver = webdriver_selenium.Remote(
+            command_executor=f"{url}/wd/hub",
+            options=options
+        )
+        browser.config.driver = driver
+        browser.config.window_width = 1920
+        browser.config.window_height = 1080
     yield browser
 
     attach.add_html(browser)
@@ -67,3 +81,42 @@ def setup_browser(request):
     attach.add_logs(browser)
     attach.add_video(browser)
     browser.quit()
+
+    # if headless:
+    #     options.add_argument('--headless')
+    #     browser.config.browser_name = 'chrome'
+    #     browser.config.hold_browser_open = (
+    #             os.getenv('selene.hold_browser_open', 'false').lower() == 'true'
+    #     )
+    #     browser.config.timeout = float(os.getenv('selene.timeout', '4'))
+    #     browser.config.window_width = 1920
+    #     browser.config.window_height = 1080
+    #     browser.quit()
+    #
+    # else:
+    #     selenoid_capabilities = {
+    #         "browserName": "chrome",
+    #         "browserVersion": browser_version,
+    #         "selenoid:options": {
+    #             "enableVNC": True,
+    #             "enableVideo": True
+    #         }
+    #     }
+    #     options.capabilities.update(selenoid_capabilities)
+    #
+    #     url = os.getenv('URL')
+    #
+    #     driver = webdriver_selenium.Remote(
+    #         command_executor=f"{url}/wd/hub",
+    #         options=options
+    #     )
+    #     browser.config.driver = driver
+    #     browser.config.window_width = 1920
+    #     browser.config.window_height = 1080
+    # yield browser
+    #
+    # attach.add_html(browser)
+    # attach.add_screenshot(browser)
+    # attach.add_logs(browser)
+    # attach.add_video(browser)
+    # browser.quit()
